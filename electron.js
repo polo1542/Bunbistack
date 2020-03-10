392b5c2a6d8cf9c8a9e9e2f8465c02eafbe394e0
polo1542 <54718858+polo1542@users.noreply.github.com>
2020-03-09 18:46:40 -0600

    Update electron.js

--- electron.js
+++ electron.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1100,
-    height: 800,
+    width: 1100, height: 800,
+    minWidth: 450, minHeight:450,
     webPreferences: {
       nodeIntegration: true
     },
